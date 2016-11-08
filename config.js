369aa4b8ac332d65b57ccc5be343b046ffae7612
Robin Millette <robin@millette.info>
2016-11-07 19:03:18 -0500

    config.js

--- config.js
+++ config.js
@@ -1,21 +1,35 @@
 'use strict'
 
 const Confidence = require('confidence')
+const criteria = { env: process.env.NODE_ENV }
 
-const criteria = {
-  env: process.env.NODE_ENV
+const defTrue = {
+  $filter: 'env',
+  prod: false,
+  $default: true
+}
+
+const defFalse = {
+  $filter: 'env',
+  prod: true,
+  $default: false
 }
 
 const config = {
   $meta: 'This file configures the plot device.',
-  projectName: 'mm-phd',
-  cache: {
-    web: {
-      $filter: 'env',
-      prod: true,
-      $default: false
-    }
+  projectName: 'hapi-demo',
+  app: { siteTitle: 'Super titre pour un super site' },
+  i18n: {
+    autoReload: defTrue,
+    updateFiles: defTrue,
+    locales: ['fr', 'en']
   },
+  db: { url: 'http://localhost:5990' },
+  cookie: {
+    password: 'password-should-be-32-characters',
+    secure: defFalse
+  },
+  cache: { web: defFalse },
   port: {
     web: {
       $filter: 'env',
@@ -26,11 +40,5 @@ const config = {
 }
 
 const store = new Confidence.Store(config)
-
-exports.get = function (key) {
-  return store.get(key, criteria)
-}
-
-exports.meta = function (key) {
-  return store.meta(key, criteria)
-}
+exports.get = (key) => store.get(key, criteria)
+exports.meta = (key) => store.meta(key, criteria)
