67585be5c38e0867b87d671c7429562b192ed9c5
Robin Millette <robin@millette.info>
2016-11-19 02:29:42 -0500

    Bit shorter.

--- ddoc/app/index.js
+++ ddoc/app/index.js
@@ -8,14 +8,13 @@ const path = require('path')
 
 const makeDdoc = () => {
   const ddoc = { _id: '_design/' + path.basename(__dirname), language: 'javascript' }
-  glob
-    .sync('+(views|updates|shows|lists)/*.js', { noDirs: true, cwd: __dirname })
-    .forEach((file) => {
-      const parts = file.split('/')
-      if (parts.length !== 2) { throw new Error('bad path in design doc') }
-      if (!ddoc[parts[0]]) { ddoc[parts[0]] = { } }
-      ddoc[parts[0]][path.basename(parts[1], '.js')] = require([__dirname, file].join('/'))
-    })
+  const wrk = (file) => {
+    const parts = file.split('/')
+    if (parts.length !== 2) { throw new Error('bad path in design doc') }
+    if (!ddoc[parts[0]]) { ddoc[parts[0]] = { } }
+    ddoc[parts[0]][path.basename(parts[1], '.js')] = require([__dirname, file].join('/'))
+  }
+  glob.sync('+(views|updates|shows|lists)/*.js', { noDirs: true, cwd: __dirname }).forEach(wrk)
   return ddoc
 }
 
