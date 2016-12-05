8d6c3d8a982a2829bb0b780dc434c99d1690b9f3
Robin Millette <robin@millette.info>
2016-12-05 16:54:54 -0500

    Fixing jsons.

--- info/add-to-couch/do.js
+++ info/add-to-couch/do.js
@@ -4,71 +4,46 @@
 const fs = require('fs')
 
 const fixJson = (filename) => new Promise((resolve, reject) => {
-  console.log('FN:', filename)
   fs.readFile(filename, 'utf-8', (err, b) => {
     if (err) { return reject(err) }
     const lines = b.split('\r\n')
-    console.log(lines.length)
 
     const a = lines
-      .filter((line) => {
-        // console.log('LL:', line.length)
-        return line.length
-      })
+      .filter((line) => line.length)
       .map((line) => {
         const lc = line.slice(-1)[0]
-        console.log('LCCCC:', lc, line.length, line)
         if (lc === ',' || lc === '{' || lc === '}') {
-          // console.log('LINE:', line)
           return line
         }
-        // console.log('LEN:', line.length, line)
         return line + ','
       })
       .join('\n')
 
-    console.log('ABCCCC:', a)
 
     let zz
-    // const yy = eval(`zz = ${a}`)
-
-    // console.log('YY:', typeof yy)
-    // console.log('ZZ:', typeof zz)
-    console.log('AAAACC:', a)
-
-    eval(`zz = ${a}`)
-    console.log('ZZCCCC:', zz)
-    // const xx1 = JSON.stringify(yy)
-    // const xx2 = JSON.stringify(zz)
-    // console.log('YYx:', xx1)
-    // console.log('ZZx:', xx2)
+    try {
+      eval(`zz = ${a}`)
+    } catch (e) {
+      e.filenane = filename
+      return reject(e)
+    }
     resolve(zz)
-    // resolve(JSON.stringify(zz, null, '  '))
-    // resolve(JSON.stringify(xx2, null, '  '))
-    // resolve(JSON.stringify(eval(`zz = ${a}`), null, '  '))
-    // resolve(JSON.stringify(eval(`zz = ${a}`), null, '  '))
   })
 })
 
-fs.readdir('wrk', (err, a) => {
-  // console.log(err, a)
-  Promise.all(a.map((fn) => fixJson(fn)))
+fs.readdir('.', (err, a) => {
+  Promise.all(
+    a
+      .filter((fn) => {
+        return fn.slice(-5) === '.json'
+      })
+      .map((fn) => fixJson(fn))
+  )
     .then((jsons) => {
-      // console.log('JSON:', JSON.stringify(jsons, null, '  '))
-      console.log('JSONOK')
+      console.log('JSON:', JSON.stringify(jsons, null, '  '))
+      // console.log('JSONOK')
     })
     .catch((err) => {
       console.error('ERR:', err)
     })
 })
-
-
-/*
-fixJson('page-2016-12-01-08-59-22-87534215684.json')
-  .then((json) => {
-    console.log(json)
-  })
-  .catch((err) => {
-    console.error(err)
-  })
-*/
