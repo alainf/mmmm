7675662321455d12dbd9f7aef7f39632bde93614
Robin Millette <robin@millette.info>
2016-12-06 16:58:56 -0500

    Deep link, step 1.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,11 @@
 /* global $ */
+'use strict'
 
-$(() => {
+$(function () {
   $(document).foundation()
-})
 
+  $('a').on('click', function (ev) {
+    console.log('this', this)
+    console.log('ev', ev)
+  })
+})
