a3c58d6e93f74a57d90a8a15d8c036f4ca2c10bc
Robin Millette <robin@millette.info>
2016-12-09 14:19:50 -0500

    Stuff view includes _id field.

--- ddoc/app/views/stuff.js
+++ ddoc/app/views/stuff.js
@@ -4,10 +4,17 @@
 module.exports = {
   map: function (doc) {
     var r
+    var g
 
     for (r in doc) {
-      if (r !== '_id' && r !== '_rev' && r !== 'apercu' && r !== 'citation' && r !== 'contenu' && r !== 'description') {
-        emit([r, doc[r]])
+      if (r !== '_rev' && r !== 'apercu' && r !== 'citation' && r !== 'contenu' && r !== 'description') {
+        if (typeof doc[r] === 'string') {
+          emit([r, doc[r]])
+        } else if (typeof doc[r] === 'object' && doc[r].length) {
+          for (g = 0; g < doc[r].length; ++g) {
+            emit([r, doc[r][g]])
+          }
+        }
       }
     }
   },
