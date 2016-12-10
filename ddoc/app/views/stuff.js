fe8ea943e4f257adb28cf98555194fdb1da65af5
Robin Millette <robin@millette.info>
2016-12-10 16:44:55 -0500

    Also show number values.

--- ddoc/app/views/stuff.js
+++ ddoc/app/views/stuff.js
@@ -8,7 +8,7 @@ module.exports = {
 
     for (r in doc) {
       if (r !== '_rev' && r !== 'apercu' && r !== 'citation' && r !== 'contenu' && r !== 'description') {
-        if (typeof doc[r] === 'string') {
+        if (typeof doc[r] === 'string' || typeof doc[r] === 'number') {
           emit([r, doc[r]])
         } else if (typeof doc[r] === 'object' && doc[r].length) {
           for (g = 0; g < doc[r].length; ++g) {
