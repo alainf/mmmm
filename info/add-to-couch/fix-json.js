f4d164488016c7f1595ede04fbe022d15ca780ec
Robin Millette <robin@millette.info>
2016-12-19 16:05:37 -0500

    Add comments fix-json.js.

--- info/add-to-couch/fix-json.js
+++ info/add-to-couch/fix-json.js
@@ -3,78 +3,105 @@
 // core
 const fs = require('fs')
 
-const fixJson = (filename) => new Promise((resolve, reject) => {
-  fs.readFile(filename, 'utf-8', (err, b) => {
-    if (err) { return reject(err) }
-    const lines = b.split('\r\n')
+/**
+ * Générer un json corrigé (ligne par ligne, dangling commas, etc.)
+ */
+const fixLine = (line) => {
+  const lc = line.slice(-1)[0]
+  if (lc === ',' || lc === '{' || lc === '}') { return line }
+  return line + ','
+}
 
-    const a = lines
+/**
+ * Transforme un json selon nos souhaits
+ */
+const fixJson = (filename) => new Promise((resolve, reject) => {
+  fs.readFile(filename, 'utf-8', (err, content) => {
+    if (err) { return reject(err) }
+
+    const json = content
+      .split('\r\n')
       .filter((line) => line.length)
-      .map((line) => {
-        const lc = line.slice(-1)[0]
-        if (lc === ',' || lc === '{' || lc === '}') { return line }
-        return line + ','
-      })
+      .map(fixLine)
       .join('\n')
 
-    let zz
+    let doc
     try {
-      eval(`zz = ${a}`) // eslint-disable-line no-eval
+      // On évalue la chaine json comme si c'était du code JavaScript
+      eval(`doc = ${json}`) // eslint-disable-line no-eval
     } catch (e) {
       e.filename = filename
       return reject(e)
     }
     let r
 
-    for (r in zz) {
-      if (zz[r] === 'true') {
-        zz[r] = true
-      } else if (zz[r] === 'false') {
-        zz[r] = false
+    for (r in doc) {
+      // true et false: bool, not strings
+      if (doc[r] === 'true') {
+        doc[r] = true
+      } else if (doc[r] === 'false') {
+        doc[r] = false
       } else {
         if (r === 'identifiant-drupal') {
-          if (typeof zz[r] === 'object') { zz[r] = zz[r][0] }
-          zz[r] = parseInt(zz[r], 10)
+          if (typeof doc[r] === 'object') { doc[r] = doc[r][0] }
+          // nombre entier
+          doc[r] = parseInt(doc[r], 10)
         } else if (r === 'niveau') {
-          zz[r] = parseInt(zz[r], 10)
+          // nombre entier
+          doc[r] = parseInt(doc[r], 10)
         } else if (r === 'importance' || r === 'pertinence') {
-          zz[r] = parseFloat(zz[r])
-        } else if (r === 'mots-clefs' && typeof zz[r] === 'object' && zz[r].length) {
-          zz[r] = zz[r].map((x) => x.trim())
+          // nombre décimal
+          doc[r] = parseFloat(doc[r])
+        } else if (r === 'mots-clefs' && typeof doc[r] === 'object' && doc[r].length) {
+          // enlever les espaces au début et à la fin des mots clés.
+          doc[r] = doc[r].map((x) => x.trim())
         }
       }
     }
-    if (!zz._id) {
-      if (typeof zz['nom-machine'] === 'string') {
-        zz._id = zz['nom-machine']
-      } else if (typeof zz['nom-machine'] === 'object' && zz['nom-machine'][0]) {
-        zz._id = zz['nom-machine'][0]
+    // Si le doc ne contient pas de champ _id
+    if (!doc._id) {
+      if (typeof doc['nom-machine'] === 'string') {
+        // utiliser le nom-machine
+        doc._id = doc['nom-machine']
+      } else if (typeof doc['nom-machine'] === 'object' && doc['nom-machine'][0]) {
+        // si nom-machine est un array, utiliser le premier terme
+        doc._id = doc['nom-machine'][0]
       } else {
+        // _id est un champ nécessaire
         return reject(new Error(`Required: either _id or nom-machine in ${filename}`))
       }
     }
-    resolve({ fn: filename, json: zz })
+    resolve({ fn: filename, json: doc })
   })
 })
 
-fs.readdir('.', (err, a) => {
-  if (err) {
-    console.error(err)
-    return
-  }
-  Promise.all(
-    a
-      .filter((fn) => fn.slice(-5) === '.json')
-      .map(fixJson)
-  )
-    .then((jsons) => {
-      console.log('jsons.length:', jsons.length)
-      return jsons
-    })
-    .then((jsons) => {
-      jsons.forEach((j) => {
-        fs.writeFile(`new/${j.fn}`, JSON.stringify(j.json, null, '  '), 'utf-8')
+/**
+ * Transformer les fichiers json du répertoire courant
+ * et sauver ça dans new/
+ */
+const processDirectory = () => {
+  fs.readdir('.', (err, file) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    Promise.all(
+      file
+        .filter((fn) => fn.slice(-5) === '.json') // seulement les .json
+        .map(fixJson) // transformer
+    )
+      .then((jsons) => {
+        console.log('jsons.length:', jsons.length)
+        return jsons
       })
-    })
-    .catch((err) => { console.error('ERR:', err) })
-})
+      .then((jsons) => {
+        // Écrire les nouveaux .json transoformés dans new/
+        jsons.forEach((j) => {
+          fs.writeFile(`new/${j.fn}`, JSON.stringify(j.json, null, '  '), 'utf-8')
+        })
+      })
+      .catch((err) => { console.error('ERR:', err) })
+  })
+}
+
+processDirectory()
