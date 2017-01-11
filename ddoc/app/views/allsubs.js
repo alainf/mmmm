e8a2fe6c37432b283c04549e443fe8e2f7343e2c
Robin Millette <robin@millette.info>
2017-01-11 07:43:27 -0500

    allsubs and allsubs2 views to find missings docs.

---
+++ ddoc/app/views/allsubs.js
@@ -0,0 +1,16 @@
+/* globals emit */
+'use strict'
+
+module.exports = {
+  map: function (doc) {
+    ['lieu-concerne', 'personne-concernee', 'voir-aussi', 'sujet', 'section']
+      .forEach(function (s) {
+        if (doc[s]) {
+          doc[s].forEach(function (docId) {
+            emit(docId)
+          })
+        }
+      })
+  },
+  reduce: '_count'
+}
