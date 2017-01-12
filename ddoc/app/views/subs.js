b6f04145652b5efe9025a14e7fc4b0de3618a02a
Robin Millette <robin@millette.info>
2017-01-12 04:18:39 -0500

    Add more sub-fields.

--- ddoc/app/views/subs.js
+++ ddoc/app/views/subs.js
@@ -3,7 +3,17 @@
 
 module.exports = {
   map: function (doc) {
-    ['lieu-edition', 'lieu-concerne', 'personne-concernee', 'voir-aussi', 'sujet', 'section']
+    [
+      'lieu-edition',
+      'lieu-concerne',
+      'personne-concernee',
+      'voir-aussi',
+      'sujet',
+      'langue-edition',
+      'site-de-diffusion',
+      'type-de-ressource',
+      'section'
+    ]
       .forEach(function (s) {
         if (doc[s]) {
           doc[s].forEach(function (docId) {
