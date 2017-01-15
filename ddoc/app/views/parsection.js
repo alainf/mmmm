3a78e7aea4e936c330f7dfaca2e70d924f1eaff8
Robin Millette <robin@millette.info>
2017-01-15 11:09:37 -0500

    Section view.

---
+++ ddoc/app/views/parsection.js
@@ -0,0 +1,12 @@
+/* globals emit */
+'use strict'
+
+module.exports = {
+  map: function (doc) {
+    if (doc.affichage === 'article') {
+      doc.section.forEach(function (x) {
+        emit([x, doc.pertinence])
+      })
+    }
+  }
+}
