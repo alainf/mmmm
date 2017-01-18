82e4e5cae2d069262e3e304c425bafe86f4b6850
Robin Millette <robin@millette.info>
2017-01-18 15:42:41 -0500

    View to top level subjects and sections.

--- ddoc/app/views/toplevels.js
+++ ddoc/app/views/toplevels.js
@@ -3,18 +3,10 @@
 
 module.exports = {
   map: function (doc) {
-    var r
-    var nom
-    var bc
     if (!doc.breadcrumb || doc.type !== 'taxonomies') { return }
-    bc = doc.breadcrumb.replace(/^\//, '').replace(/\/$/, '').split('/')
+    var bc = doc.breadcrumb.replace(/^\//, '').replace(/\/$/, '').split('/')
     if (bc[0] !== 'taxonomies' || bc.length !== 3) { return }
-    // if (bc[1] !== 'sections' && bc[1] !== 'sujets') { return }
-    emit([bc[1], doc._id], doc.nomLangues)
-    if (!doc.enfants || !doc.enfants.length) { return }
-    doc.enfants.forEach(function (enfant) {
-      emit([bc[1], doc._id, 'enfant'], { _id: enfant })
-    })
+    emit([bc[1], doc._id, typeof doc.nomLangues.fr === 'string' ? doc.nomLangues.fr : doc.nomLangues.fr[0]], doc.nomLangues)
   },
   reduce: '_count'
 }
