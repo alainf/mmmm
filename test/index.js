e27186175c13b515db0dbac1db6f09df815c6066
Robin Millette <robin@millette.info>
2017-05-28 06:45:08 -0400

    Remove debugging output.

--- test/index.js
+++ test/index.js
@@ -6,14 +6,9 @@ const Composer = require('../index')
 
 const lab = exports.lab = Lab.script()
 
-let cntA = 0
-let cntB = 0
-
 lab.experiment('App', () => {
   lab.test('it composes a server', (done) => {
-    console.log('cntA', ++cntA)
     Composer((err, composedServer) => {
-      console.log('cntB', ++cntB)
       Code.expect(composedServer).to.be.an.object()
       done(err)
     })
